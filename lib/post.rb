af3a27cee602b8522347c469d11bbb7d4f6acd5b
Matt Im <matthew.seiim@gmail.com>
2020-05-11 21:37:43 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -17,7 +17,7 @@ attr_accessor :title, :author
     # Post.all.select do |post|
     #   post.author_name == self 
     # end
-    Post.all.select do |author_name|
+   
     post.author_name
   end
   end
