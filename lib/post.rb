7ece3bd85c883f6e51b4949aecc6c57e06ace55e
Matt Im <matthew.seiim@gmail.com>
2020-05-11 21:23:46 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post 
+
+@@all = []
+
+attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def author_name 
+    Post.all.select do |post|
+      post.author_name == self 
+    end
+  end
+end
